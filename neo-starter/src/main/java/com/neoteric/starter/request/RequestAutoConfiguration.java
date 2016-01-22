f1fd9c6abe04146dccbd4ae5ddfae8caf334ffb6
dziesio <gpoznachowski@neoteric.eu>
2016-01-22 10:33:22 +0100

    Remove RISON implementation - Probably not required and has bad quality of code

--- neo-starter/src/main/java/com/neoteric/starter/request/RequestAutoConfiguration.java
+++ neo-starter/src/main/java/com/neoteric/starter/request/RequestAutoConfiguration.java
@@ -1,8 +1,6 @@
 package com.neoteric.starter.request;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.neoteric.starter.Constants;
-import com.neoteric.starter.jackson.rison.RisonFactory;
 import com.neoteric.starter.jersey.JerseyDefaultsAutoConfiguration;
 import com.neoteric.starter.request.params.RequestParametersFilter;
 import com.neoteric.starter.request.tracing.RequestIdFilter;
@@ -18,44 +16,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({RequestProperties.class, JerseyProperties.class})
+@EnableConfigurationProperties(JerseyProperties.class)
 @AutoConfigureAfter({JacksonAutoConfiguration.class, JerseyDefaultsAutoConfiguration.class})
 public class RequestAutoConfiguration {
 
     private static final Logger LOG = LoggerFactory.getLogger(RequestAutoConfiguration.class);
 
     @Autowired
-    RequestProperties requestProperties;
-
-    @Autowired
     JerseyProperties jerseyProperties;
-
-    @Autowired
-    ObjectMapper objectMapper;
 
     @Bean
     FilterRegistrationBean registerRequestIdFilter() {
         return new FilterRegistrationBean(new RequestIdFilter(jerseyProperties.getApplicationPath()));
     }
 
-    @Bean //TODO: Check if Rison is worth supporting
-    FilterRegistrationBean registerRequestParamsFilter() throws Exception {
-        RequestProperties.FiltersFormat filtersFormat = requestProperties.getFiltersFormat();
-        ObjectMapper requestMapper;
-        switch(filtersFormat) {
-            case RISON:
-                LOG.trace("{}Picked RISON Mapper", Constants.LOG_PREFIX);
-                requestMapper = new ObjectMapper(new RisonFactory());
-                break;
-            case JSON:
-                LOG.trace("{}Picked default JSON Mapper", Constants.LOG_PREFIX);
-                requestMapper = objectMapper;
-                break;
-            default:
-                LOG.error("{}{} format not supported.", Constants.LOG_PREFIX, filtersFormat);
-                throw new Exception(filtersFormat + " not supported.");
-        }
-
-        return new FilterRegistrationBean(new RequestParametersFilter(requestMapper, jerseyProperties.getApplicationPath()));
+    @Bean
+    FilterRegistrationBean registerRequestParamsFilter(ObjectMapper objectMapper) throws Exception {
+        return new FilterRegistrationBean(new RequestParametersFilter(objectMapper, jerseyProperties.getApplicationPath()));
     }
 }
