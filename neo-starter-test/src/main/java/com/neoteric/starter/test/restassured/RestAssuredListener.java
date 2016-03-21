be508ba1feb68e347808f5b573e6525511411d49
dziesio <grzegorz@poznachowski.pl>
2016-03-21 12:04:48 +0100

    Make RestAssuredListener find ContainerIntegrationtest annotation, as it does not need to be explicitly set on Test class

--- neo-starter-test/src/main/java/com/neoteric/starter/test/restassured/RestAssuredListener.java
+++ neo-starter-test/src/main/java/com/neoteric/starter/test/restassured/RestAssuredListener.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.config.ObjectMapperConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.support.AbstractTestExecutionListener;
 
@@ -14,7 +15,7 @@ public class RestAssuredListener extends AbstractTestExecutionListener {
 
     @Override
     public void beforeTestClass(TestContext testContext) throws Exception {
-        if (testContext.getTestClass().getAnnotation(ContainerIntegrationTest.class) == null) {
+        if (AnnotationUtils.findAnnotation(testContext.getTestClass(), ContainerIntegrationTest.class) == null) {
             return;
         }
         ApplicationContext applicationContext = testContext.getApplicationContext();
