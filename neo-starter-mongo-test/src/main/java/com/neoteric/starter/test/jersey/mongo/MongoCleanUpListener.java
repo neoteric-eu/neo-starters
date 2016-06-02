96b43fd2a24ab1f0195299246e32d5ac9d6b172a
kmi <kmiotk@ntrc.eu>
2016-06-02 12:10:53 +0200

    Added clean collection annotation for embedded mongo integration tests

--- neo-starter-mongo-test/src/main/java/com/neoteric/starter/test/jersey/mongo/MongoCleanUpListener.java
+++ neo-starter-mongo-test/src/main/java/com/neoteric/starter/test/jersey/mongo/MongoCleanUpListener.java
@@ -1,14 +1,16 @@
 package com.neoteric.starter.test.jersey.mongo;
 
+import com.mongodb.BasicDBObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.core.Ordered;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.support.AbstractTestExecutionListener;
 
 import java.util.Arrays;
+import java.util.function.Consumer;
 
 public class MongoCleanUpListener extends AbstractTestExecutionListener {
 
@@ -16,10 +18,12 @@ public class MongoCleanUpListener extends AbstractTestExecutionListener {
 
     @Override
     public void afterTestMethod(TestContext testContext) throws Exception {
-        DropCollections annotation = testContext.getTestClass().getAnnotation(DropCollections.class);
+        ClearCollections annotation = AnnotationUtils.findAnnotation(testContext.getTestClass(), ClearCollections.class);
+
         if (annotation == null) {
             return;
         }
+
         MongoTemplate mongoTemplate;
         try {
             mongoTemplate = testContext.getApplicationContext().getBean("mongoTemplate", MongoTemplate.class);
@@ -27,11 +31,47 @@ public class MongoCleanUpListener extends AbstractTestExecutionListener {
             LOG.warn("mongoTemplate bean not found. Skipping collections cleanup.", e);
             return;
         }
-        Arrays.stream(annotation.value()).forEach(mongoTemplate::dropCollection);
+
+        Consumer<String> consumer = annotation.drop() ? DropConsumer.of(mongoTemplate) : ClearConsumer.of(mongoTemplate);
+        Arrays.stream(annotation.value()).forEach(consumer);
     }
 
     @Override
     public int getOrder() {
-        return Ordered.HIGHEST_PRECEDENCE;
+        return HIGHEST_PRECEDENCE;
+    }
+}
+
+class DropConsumer implements Consumer<String> {
+    private final MongoTemplate mongoTemplate;
+
+    static Consumer of(MongoTemplate mongoTemplate) {
+        return new DropConsumer(mongoTemplate);
+    }
+
+    private DropConsumer(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Override
+    public void accept(String collection) {
+        mongoTemplate.getCollection(collection).drop();
+    }
+}
+
+class ClearConsumer implements Consumer<String> {
+    private final MongoTemplate mongoTemplate;
+
+    static Consumer of(MongoTemplate mongoTemplate) {
+        return new ClearConsumer(mongoTemplate);
+    }
+
+    private ClearConsumer(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Override
+    public void accept(String collection) {
+        mongoTemplate.getCollection(collection).remove(new BasicDBObject());
     }
 }
