bd8d4c319dbfc79c3bf9a3bea166672ebd51651f
dziesio <grzegorz@poznachowski.pl>
2016-06-08 11:11:51 +0200

    Stop using deprecated FilterRegistrationBean

--- neo-starter/src/main/java/com/neoteric/starter/request/params/RequestParamsAutoConfiguration.java
+++ neo-starter/src/main/java/com/neoteric/starter/request/params/RequestParamsAutoConfiguration.java
@@ -2,12 +2,11 @@ package com.neoteric.starter.request.params;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.neoteric.starter.mvc.StarterMvcProperties;
-import com.neoteric.starter.request.params.RequestParametersFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
