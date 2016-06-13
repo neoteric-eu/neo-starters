c4606a1ad8651adcef3ddbf999cd18fcb9cdf0b6
dziesio <grzegorz@poznachowski.pl>
2016-06-13 14:59:35 +0200

    Sonar fixes

--- neo-starter-saasmgr-test/src/main/java/com/neoteric/starter/test/saasmgr/mockmvc/WithSaasMgrPrincipalContextFactory.java
+++ neo-starter-saasmgr-test/src/main/java/com/neoteric/starter/test/saasmgr/mockmvc/WithSaasMgrPrincipalContextFactory.java
@@ -4,6 +4,7 @@ import com.neoteric.starter.saasmgr.auth.SaasMgrAuthenticationToken;
 import com.neoteric.starter.saasmgr.model.SubscriptionConstraint;
 import com.neoteric.starter.saasmgr.principal.DefaultSaasMgrPrincipal;
 import com.neoteric.starter.saasmgr.principal.SaasMgrPrincipal;
+import com.neoteric.starter.test.saasmgr.AuthenticationTokenHelper;
 import org.assertj.core.util.Lists;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,6 +17,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.neoteric.starter.test.saasmgr.AuthenticationTokenHelper.anonymousToken;
+import static com.neoteric.starter.test.saasmgr.AuthenticationTokenHelper.getConstraints;
+
 public class WithSaasMgrPrincipalContextFactory implements
 		WithSecurityContextFactory<WithSaasMgrPrincipal> {
 
@@ -24,8 +28,7 @@ public class WithSaasMgrPrincipalContextFactory implements
         SecurityContext context = SecurityContextHolder.createEmptyContext();
         Authentication token;
         if (annotation.noAuth()) {
-            token = new AnonymousAuthenticationToken(
-                    "anonymousUser", "anonymousUser", Lists.newArrayList(new SimpleGrantedAuthority("ROLE_ANONYMOUS")));
+            token = anonymousToken();
         } else {
             SaasMgrPrincipal principal = DefaultSaasMgrPrincipal.builder()
                     .customerId(annotation.customerId())
@@ -43,14 +46,4 @@ public class WithSaasMgrPrincipalContextFactory implements
         context.setAuthentication(token);
         return context;
     }
-
-	private List<SubscriptionConstraint> getConstraints(String[] constraints) {
-		return Arrays
-				.stream(constraints)
-				.map(con -> {
-					String[] splitted = con.split(";");
-					return new SubscriptionConstraint(splitted[0], Double
-							.valueOf(splitted[1]), Double.valueOf(splitted[2]));
-				}).collect(Collectors.toList());
-	}
 }
